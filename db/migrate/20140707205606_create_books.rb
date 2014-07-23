f5308b9b009523d644014b0a11a05c002896a1ef
& <pair+benbrostoff.greg99799@devbootcamp.com>
2014-07-23 19:46:58 -0400

    Added book with sentences to database

--- db/migrate/20140707205606_create_books.rb
+++ db/migrate/20140707205606_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :title
-      t.text :sentences
+      t.text :sentences, array: true
 
       t.timestamps
     end
